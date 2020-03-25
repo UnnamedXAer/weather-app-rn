afc71176a999ac97e0c62bfd3577565f04908377
Kamil <unnamedxaer@gmail.com>
2020-03-25 18:30:03 +0100

    Clear files / assets

--- axios/axios.ts
+++ axios/axios.ts
@@ -1,14 +1,9 @@
 import Axios, { AxiosError } from 'axios';
 import { REACT_APP_PROXY_URL } from '../env';
 
-
-console.log(REACT_APP_PROXY_URL);
-
-
 const axios = Axios.create({
-	baseURL:REACT_APP_PROXY_URL ,
-	// baseURL: 'http://192.168.1.5:3390/',
-		withCredentials: true
+	baseURL: REACT_APP_PROXY_URL,
+	withCredentials: true
 });
 
 export const logReqError = (err: AxiosError) => {
