447662baf9e305bd48a73f3fcffa16089f2fd426
Kamil <unnamedxaer@gmail.com>
2020-03-25 18:27:10 +0100

    Use env file for settings

--- axios/axios.ts
+++ axios/axios.ts
@@ -1,10 +1,14 @@
 import Axios, { AxiosError } from 'axios';
+import { REACT_APP_PROXY_URL } from '../env';
+
+
+console.log(REACT_APP_PROXY_URL);
+
 
 const axios = Axios.create({
-	// baseURL: 'https://weather-app-proxy-js.herokuapp.com/',
-	// baseURL: process.env.REACT_APP_PROXY_URL,
-	baseURL: 'http://192.168.1.5:3390/',
-	withCredentials: true
+	baseURL:REACT_APP_PROXY_URL ,
+	// baseURL: 'http://192.168.1.5:3390/',
+		withCredentials: true
 });
 
 export const logReqError = (err: AxiosError) => {
